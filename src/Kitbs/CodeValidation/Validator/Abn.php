34a5ab263322bb10871e75c89581f9240e982227
KitBS <mail@kitbs.com>
2014-11-13 20:47:10 -0500

    indenting Abn.php

--- src/Kitbs/CodeValidation/Validator/Abn.php
+++ src/Kitbs/CodeValidation/Validator/Abn.php
@@ -15,8 +15,8 @@ use IsoCodes\IsoCodeInterface;
  */
 
 class Abn implements IsoCodeInterface {
-	
-    /**
+
+	/**
      * ABN validation
      *
      * @param string $abn The ABN
@@ -24,63 +24,63 @@ class Abn implements IsoCodeInterface {
      * @return bool
      */
 	public static function validate($abn)
-    {
- 
-        $abn = str_replace(" ", "", $abn);
-        $abnLen = strlen($abn);
- 
-        if (is_numeric($abn) && $abnLen == 11) {
- 
-            $sum = 0;
- 
-            // Multiply each of the digits by its weighting factor, summing the products        
-            for ($position=0; $position < $abnLen; $position++) {
- 
-                // Get the value of the current digit
-                $current = intval($abn[$position]);
- 
-                switch ($position+1) {
-                    case 1:
-                    $sum = $sum + (10 * ($current - 1));
-                    break;
-                    case 2:
-                    $sum = $sum + (1 * $current);
-                    break;
-                    case 3:
-                    $sum = $sum + (3 * $current);
-                    break;
-                    case 4:
-                    $sum = $sum + (5 * $current);
-                    break;
-                    case 5:
-                    $sum = $sum + (7 * $current);
-                    break;
-                    case 6:
-                    $sum = $sum + (9 * $current);
-                    break;
-                    case 7:
-                    $sum = $sum + (11 * $current);
-                    break;
-                    case 8:
-                    $sum = $sum + (13 * $current);
-                    break;
-                    case 9:
-                    $sum = $sum + (15 * $current);
-                    break;
-                    case 10:
-                    $sum = $sum + (17 * $current);
-                    break;
-                    case 11:
-                    $sum = $sum + (19 * $current);
-                    break;
-                }
- 
-            }
-            //If total is exactly divisble by 89 then it is an ABN
-            return ($sum % 89 == 0);
-        }
- 
-        return false;
-    }
-	
+	{
+
+		$abn = str_replace(" ", "", $abn);
+		$abnLen = strlen($abn);
+
+		if (is_numeric($abn) && $abnLen == 11) {
+
+			$sum = 0;
+
+			// Multiply each of the digits by its weighting factor, summing the products        
+			for ($position=0; $position < $abnLen; $position++) {
+
+				// Get the value of the current digit
+				$current = intval($abn[$position]);
+
+				switch ($position+1) {
+					case 1:
+					$sum = $sum + (10 * ($current - 1));
+					break;
+					case 2:
+					$sum = $sum + (1 * $current);
+					break;
+					case 3:
+					$sum = $sum + (3 * $current);
+					break;
+					case 4:
+					$sum = $sum + (5 * $current);
+					break;
+					case 5:
+					$sum = $sum + (7 * $current);
+					break;
+					case 6:
+					$sum = $sum + (9 * $current);
+					break;
+					case 7:
+					$sum = $sum + (11 * $current);
+					break;
+					case 8:
+					$sum = $sum + (13 * $current);
+					break;
+					case 9:
+					$sum = $sum + (15 * $current);
+					break;
+					case 10:
+					$sum = $sum + (17 * $current);
+					break;
+					case 11:
+					$sum = $sum + (19 * $current);
+					break;
+				}
+
+			}
+			//If total is exactly divisble by 89 then it is an ABN
+			return ($sum % 89 == 0);
+		}
+
+		return false;
+	}
+
 }
